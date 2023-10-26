56e953bdc43f32141081aeade7aa6cfd82268ec5
Justin Chhay <jchhay@uwaterloo.ca>
2023-10-25 21:58:22 -0400

    grab spotify access token

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, InputGroup, FormControl, Button, Row, Card, CardBody} from 'react-bootstrap';
 import { useState, useEffect } from 'react'
 
+const CLIENT_ID = 'ca073cbf6c134b2ab15feffa2b103ff5'
+const CLIENT_SECRET = 'd280b618b12e4539859ab212fa633183'
 function App() {
   const [ searchInput, setSearchInput ] = useState("");
+
+  useEffect(() => {
+    //API Access Token
+    var authParams = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+
+    }
+    fetch('http://accounts.spotify.com/api/token', authParams)
+      .then(result => result.json())
+      .then(data => console.log(data))
+  }, [])
 
   return (
     <div className="App">
