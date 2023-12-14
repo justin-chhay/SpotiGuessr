96311fbef449e1761d41e6a1d9e704cc476078a8
justin-chhay <jchhay@uwaterloo.ca>
2023-12-13 21:44:15 -0500

    add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+﻿//if logged in
+
+
+const Navbar = () => {
+    return (
+      <nav>
+        <div>
+            <logo>SpotifyLogo Here</logo>
+        </div>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>Songs</li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+  
+  export default Navbar
