5831b2b709dd281eed8243ca2b90e80a1a09a115
justin-chhay <jchhay@uwaterloo.ca>
2023-12-14 01:12:11 -0500

    clean up styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Routes, Route} from 'react-router-dom'
-import React from 'react'
-import Home from './pages/Home'
-import Search from './pages/Search'
-import Login from './pages/Login'
-import Game from './pages/Game'
-import Settings from './pages/Settings'
+import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Login from './pages/Login';
+import Game from './pages/Game';
+import Settings from './pages/Settings';
 
 function App() {
-
   return (
-    <React.Fragment>
-      <div>
-      <Routes>
-        <Route path='/' element={<Login/>} />
-        <Route path='/home' element={<Home/>} />
-        <Route path='/search' element={<Search/>} />
-        <Route path='/game' element={<Game/>} />
-        <Route path='/settings' element={<Settings/>} />
-      </Routes>
+    <div className="relative flex h-screen bg-black">
+      <div className="flex-1">
+        <div className="h-full pb-40">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/game" element={<Game />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
       </div>
-    </React.Fragment>
-  )
-};
+    </div>
+  );
+}
 
 export default App;
