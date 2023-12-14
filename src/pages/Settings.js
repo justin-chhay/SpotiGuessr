45ad6e7d96ae15a2562fa8733c12c1b6b18cd4c2
justin-chhay <jchhay@uwaterloo.ca>
2023-12-13 21:44:42 -0500

    add new pages

---
+++ src/pages/Settings.js
@@ -0,0 +1,20 @@
+﻿import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, InputGroup, FormControl, Button, Row, Card, CardBody} from 'react-bootstrap';
+import { useState, useEffect } from 'react'
+
+const Settings = () => {
+  useEffect(() => {
+    });
+
+
+    return (
+    <div className="App">
+        <Container>
+          <h3 className="text-3xl font-bold underline">Settings</h3>
+        </Container> 
+    </div>
+    )
+}
+
+export default Settings;
